aa7ee594d9fcdbf090f3ae2e2447ceae9887c8a8
Lamoureux <Lamoureux@pc107-13.bts.lan>
2016-09-07 11:20:25 +0200

    coucou

--- src/test/java/git/TestJavaGit.java
+++ src/test/java/git/TestJavaGit.java
@@ -16,7 +16,7 @@ public class TestJavaGit {
      */
     public static void main(String[] args) {
         System.out.println("azeaze");
-        System.out.println("azeaze");
+        uju$^p^mçrti
     }
     
 }
